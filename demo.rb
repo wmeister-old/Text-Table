8031e19778be84d37be35c3b3660931bf963e98f
Willaim Meister <wmeister86@gmail.com>
2010-01-03 19:21:26 -0500

    updated comments

--- demo.rb
+++ demo.rb
@@ -11,7 +11,19 @@ test1 =   %w[name   gender], # first argument is an array representing the heade
 t = Table.new(*test1)
 
 # print the table
-t.print
+t.print # outputs:
+
+=begin
+
+  +-----------------+
+  | name   | gender |
+  |--------+--------|
+  | billy  | male   |
+  | johnny | male   |
+  | mom    | female |
+  +-----------------+
+
+=end
 
 # rebuild the table and print it
 t.make(%w[shoe       size color],
@@ -20,4 +32,18 @@ t.make(%w[shoe       size color],
        %w[vans       7    tan  ],
        %w[adidas     9    white],
        %w[timberland 12   brown],
-       %w[sketchers  6    green]).print
+       %w[sketchers  6    green]).print # outputs:
+
+=begin
+
+  +---------------------------+
+  | shoe       | size | color |
+  |------------+------+-------|
+  | nike       | 11   | black |
+  | vans       | 7    | tan   |
+  | adidas     | 9    | white |
+  | timberland | 12   | brown |
+  | sketchers  | 6    | green |
+  +---------------------------+
+
+=end
